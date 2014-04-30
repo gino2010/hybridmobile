5cd40f0fbacbf73d965e6635d5eb7810019a2bb2
Gino <ginozhao2010@gmail.com>
2014-04-30 15:28:16 +0800

    data service

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-function firstCtrl($scope){
-    $scope.message = "first"
+var myApp = angular.module('myApp', []);
+
+myApp.factory('Data', function(){
+    return {message: "I'm data from a service"};
+})
+
+function firstCtrl($scope, Data){
+    $scope.data = Data;
 }
 
-function secondCtrl($scope){
-    $scope.message = "second"
+function secondCtrl($scope, Data){
+    $scope.data = Data;
 }
