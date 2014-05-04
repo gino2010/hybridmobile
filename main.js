0b55bbc870560464d4cb271e92c7e4ce9e0de414
Gino <ginozhao2010@gmail.com>
2014-05-04 14:10:48 +0800

    directive

--- main.js
+++ main.js
@@ -1,72 +1,35 @@
-var myApp = angular.module('myApp', []);
+var app = angular.module("supperhero", []);
 
-myApp.factory('Avengers', function () {
-    var Avengers = {};
-    Avengers.cast = [
-        {
-            name: 'Robert Downey Jr.',
-            character: 'Tony Stark / Iron Man'
-        },
-        {
-            name: 'Chris Evans',
-            character: 'Steve Rogers / Captain America'
-        },
-        {
-            name: 'Mark Ruffalo',
-            character: 'Bruce Banner / The Hulk'
-        },
-        {
-            name: 'Chris Hemsworth',
-            character: 'Thor'
-        },
-        {
-            name: 'Scarlett Johansson',
-            character: 'Natasha Romanoff / Black Widow'
-        },
-        {
-            name: 'Jeremy Renner',
-            character: 'Clint Barton / Hawkeye'
-        },
-        {
-            name: 'Tom Hiddleston',
-            character: 'Loki'
-        },
-        {
-            name: 'Clark Gregg',
-            character: 'Agent Phil Coulson'
-        },
-        {
-            name: 'Cobie Smulders',
-            character: 'Agent Maria Hill'
-        },
-        {
-            name: 'Stellan Skarsgård',
-            character: 'Selvig'
-        },
-        {
-            name: 'Samuel L. Jackson',
-            character: 'Nick Fury'
-        },
-        {
-            name: 'Gwyneth Paltrow',
-            character: 'Pepper Potts'
-        },
-        {
-            name: 'Paul Bettany',
-            character: 'Jarvis (voice)'
-        },
-        {
-            name: 'Alexis Denisof',
-            character: 'The Other'
-        },
-        {
-            name: 'Tina Benko',
-            character: 'NASA Scientist'
+app.directive("supperman", function () {
+    return{
+        restrict: "E",
+        template: "<div>Here I am to save the day</div>"
+    }
+})
+
+app.directive("flash", function () {
+    return{
+        restrict: "A",
+        link: function () {
+            alert("I'm working")
         }
-    ];
-    return Avengers;
-});
+    }
+})
 
-function AvengersCtrl($scope, Avengers) {
-    $scope.avengers = Avengers;
-}
+app.directive("huck", function () {
+    return{
+        restrict: "C",
+        link: function () {
+            alert("I'm stronger")
+        }
+    }
+})
+
+app.directive("comm", function () {
+    return{
+        restrict: "M",
+        link: function () {
+            alert("I'm comment")
+        }
+    }
+})
