cdd76e7afcae4d8ea973e064d3ae60eea814eda2
Gino <ginozhao2010@gmail.com>
2014-05-06 11:15:00 +0800

    transclusion

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-var app = angular.module('phoneApp', []);
+var app = angular.module("phoneApp", []);
 
-app.controller("AppCtrl", function($scope){
-    $scope.leaveVoicemail = function(number, message){
-        alert("Number:" + number + " said: " + message);
-    }
-})
+app.controller("AppCtrl", function ($scope) {
 
-app.directive("phone", function(){
+});
+
+app.directive("panel", function() {
     return {
-        restrict: 'E',
-        scope:{
-            number: "@",
-            networkp: "=",
-            makeCall: "&"
-        },
-        template: '<div class="panel">Number: {{number}} Network: <select ng-model="networkp" ng-options="net for net in networks"></div>' +
-            '<input type="text" ng-model="value"/>' +
-            '<div class="button" ng-click="makeCall({number: number, message:value})">Call home!</div>',
-        link: function (scope) {
-            scope.networks = ["Verizon","AT&T", "Sprint"];
-            scope.networkp = scope.networks[0];
-        }
+        restrict: "E",
+        transclude: true,
+        template: '<div class="panel">This is a panel component <div ng-transclude></div></div>'
     };
 });
