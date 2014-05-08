0a5f3faa4809bb5bcd683d89285aef53b793cbe3
Gino <ginozhao2010@gmail.com>
2014-05-08 16:55:37 +0800

    Providers

--- main.js
+++ main.js
@@ -1,53 +1,25 @@
-var app = angular.module("app", ['ngRoute']);
+var app = angular.module("app", []);
 
-app.config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/',
-        {
-            templateUrl: "app.html",
-            controller: "ViewCtrl"
-        })
-        .when('/new',
-        {
-            templateUrl: "new.html",
-            controller: "NewCtrl",
-            resolve: {
-                loadData: viewCtrl.loadData
+app.provider("game", function () {
+    var type;
+    return{
+        setType: function (value) {
+            type = value;
+        },
+
+        $get: function () {
+            return {
+                title: type + "Craft"
             }
-        });
-});
+        }
+    }
+})
 
-app.controller("AppCtrl", function ($scope, $rootScope, $route, $location) {
-    $rootScope.$on("$routeChangeStart", function (event, current, previous, rejection) {
-        // Step 2, route change starts, no promises have been resolved yet
-        console.log($scope, $rootScope, $route, $location);
-    });
-    $rootScope.$on("$routeChangeSuccess", function (event, current, previous, rejection) {
-        // Step 3, route is successfully changed, UI has not been updated yet
-        console.log($scope, $rootScope, $route, $location);
-    });
-});
+app.config(function (gameProvider) {
+    gameProvider.setType("Puzzle")
 
-var viewCtrl = app.controller("ViewCtrl", function ($scope, $rootScope, $route, $location) {
-    $scope.model = {
-        message: 'This is my app'
-    };
-    $scope.changeRoute = function () {
-        // Step 1, occurs before the route change begins
-        console.log($scope);
-        $location.path("/new");
-    };
-});
+})
 
-app.controller("NewCtrl", function ($scope, loadData, $template) {
-    // Step 4, the new controller has been instantiated
-    console.log($scope, loadData, $template);
+app.controller("AppCtrl", function ($scope, game) {
+    $scope.title = game.title;
 });
-
-viewCtrl.loadData = function ($q, $timeout) {
-    var defer = $q.defer();
-    $timeout(function() {
-        defer.resolve({message: "success"});
-    }, 2000);
-    return defer.promise;
-};
