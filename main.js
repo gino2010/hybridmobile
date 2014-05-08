fc0d4414c55967e744f5ffad99702fc6309b481a
Gino <ginozhao2010@gmail.com>
2014-05-08 16:36:18 +0800

    Route Life Cycle

--- main.js
+++ main.js
@@ -1,49 +1,53 @@
 var app = angular.module("app", ['ngRoute']);
 
-app.config(function ($routeProvider) {
+app.config(function ($routeProvider, $locationProvider) {
     $routeProvider
-        .when('/', {
+        .when('/',
+        {
             templateUrl: "app.html",
-            controller: "ViewCtrl",
+            controller: "ViewCtrl"
+        })
+        .when('/new',
+        {
+            templateUrl: "new.html",
+            controller: "NewCtrl",
             resolve: {
                 loadData: viewCtrl.loadData
             }
         });
-
 });
 
-app.directive("error", function($rootScope){
-    return {
-        restrict: "E",
-        template: '<div class="alert-box alert" ng-show="isError">Error!!!!</div>',
-        link: function(scope) {
-            $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
-                scope.isError = true;
-            });
-        }
-    }
-})
-
-app.controller('AppCtrl', function ($rootScope) {
-    $rootScope.$on("$routeChangeError", function (event, current, previous, rejection) {
-        console.log(rejection);
-        $rootScope.message = rejection;
-        alert($rootScope.message);
+app.controller("AppCtrl", function ($scope, $rootScope, $route, $location) {
+    $rootScope.$on("$routeChangeStart", function (event, current, previous, rejection) {
+        // Step 2, route change starts, no promises have been resolved yet
+        console.log($scope, $rootScope, $route, $location);
+    });
+    $rootScope.$on("$routeChangeSuccess", function (event, current, previous, rejection) {
+        // Step 3, route is successfully changed, UI has not been updated yet
+        console.log($scope, $rootScope, $route, $location);
     });
 });
 
-var viewCtrl = app.controller("ViewCtrl", function ($scope, $route) {
-    console.log($route);
+var viewCtrl = app.controller("ViewCtrl", function ($scope, $rootScope, $route, $location) {
     $scope.model = {
         message: 'This is my app'
     };
+    $scope.changeRoute = function () {
+        // Step 1, occurs before the route change begins
+        console.log($scope);
+        $location.path("/new");
+    };
+});
+
+app.controller("NewCtrl", function ($scope, loadData, $template) {
+    // Step 4, the new controller has been instantiated
+    console.log($scope, loadData, $template);
 });
 
 viewCtrl.loadData = function ($q, $timeout) {
     var defer = $q.defer();
-    $timeout(function () {
-        defer.reject("your network is down");
-//        defer.resolve();
+    $timeout(function() {
+        defer.resolve({message: "success"});
     }, 2000);
     return defer.promise;
 };
