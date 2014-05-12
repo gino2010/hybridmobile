8304828663141d56f8ef63d8b5cd5bec67c2e324
Gino <ginozhao2010@gmail.com>
2014-05-12 16:21:19 +0800

    animation with angular

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
 var app = angular.module("app", ["ngAnimate"]);
 
-app.controller("AppCtrl", function(){
-    this.toggle = true;
-})
+app.controller("AppCtrl", function () {
+    this.isHidden = false;
+    this.fadeIt = function () {
+        this.isHidden = !this.isHidden;
+    }
+});
 
+$("#my-button").click(function () {
+//    TweenMax.to($("#my-badge"), 1, {opacity:0})
+});
 
-app.animation(".toggle", function(){
-    return{
-        leave: function(element, done){
-            TweenMax.fromTo(element, 1, {opacity: 1}, {opacity: 0, onComplete:done});
+app.directive("hideMe", function ($animate) {
+    return function (scope, element, attrs) {
+        scope.$watch(attrs.hideMe, function (newVal) {
+            if (newVal) {
+                $animate.addClass(element, "fade")
+            }else{
+                $animate.removeClass(element, "fade")
+            }
+        })
+    }
+});
+
+app.animation(".fade", function(){
+    return {
+        addClass: function(element, className){
+            TweenMax.to(element, 1, {opacity: 0})
         },
-
-        enter: function(element, done){
-            TweenMax.fromTo(element, 1, {opacity: 0}, {opacity: 1, onComplete:done});
+        removeClass: function(element, className){
+            TweenMax.to(element, 1, {opacity: 1})
         }
+
     }
 })
