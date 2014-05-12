254537424a78278dd045aa29458addc3a7733602
Gino <ginozhao2010@gmail.com>
2014-05-12 11:28:32 +0800

    Experiment of controller

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 var app = angular.module("app", []);
 
-app.config(function ($logProvider) {
-    $logProvider.debugEnabled(false);
-})
-
-app.run(function($rootScope, $log){
-    $rootScope.$log = $log;
-})
-
-app.controller("foo", function($scope) {
-    $scope.myFunc = function (ev) {
-        console.warn(ev)
+app.controller("RoomCtrl", function() {
+    this.openDoor = function () {
+        alert("creak");
     }
-})
+
+    this.buttonTitle = "I'm a button";
+
+    this.foo = "I'm in the room controller!";
+});
+
+app.controller('OtherCtrl', function () {
+    this.foo = "I'm in the other controller!";
+});
