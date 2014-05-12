b2f0791f75e6a5c07b0c6d7de21f7f59ffcce654
Gino <ginozhao2010@gmail.com>
2014-05-12 14:51:31 +0800

    animation basics

--- main.js
+++ main.js
@@ -1,41 +1,5 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngAnimate"]);
 
-app.directive("country", function () {
-    return {
-        restrict: "E",
-        controller: function () {
-            this.makeAnnouncement = function (message) {
-                console.log("Country says:" + message);
-            }
-        }
-    }
-});
-
-app.directive("state", function () {
-    return {
-        restrict: "E",
-        controller: function () {
-            this.makeLaw = function (law) {
-                console.log("Law: " + law)
-            }
-        },
-        link: function (scope, element, attrs, countryCtrl) {
-
-        }
-    }
-});
-
-app.directive("city", function () {
-    return {
-        restrict: "E",
-        require: ["^country", "^state"],
-        controller: function ($scope) {
-            $scope.things = ['one', 'two', 'three'];
-        },
-        link: function (scope, element, attrs, ctrls) {
-            ctrls[0].makeAnnouncement("This city rocks.");
-            ctrls[1].makeLaw("Jumper higher");
-        }
-    }
-});
-
+app.controller("AppCtrl", function(){
+    this.toggle = true;
+})
