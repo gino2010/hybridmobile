f46b7b2398ce10c1e5a2914e0dce1cc9af90c59f
Gino <ginozhao2010@gmail.com>
2014-05-12 12:20:47 +0800

    drectives communicate

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
 var app = angular.module("app", []);
 
-app.controller("RoomCtrl", function() {
-    this.openDoor = function () {
-        alert("creak");
+app.directive("country", function() {
+    return {
+        restrict: "E",
+        controller: function(){
+            this.makeAnnouncement = function (message) {
+                console.log("Country says:" + message);
+            }
+        }
     }
-
-    this.buttonTitle = "I'm a button";
-
-    this.foo = "I'm in the room controller!";
 });
 
-app.controller('OtherCtrl', function () {
-    this.foo = "I'm in the other controller!";
+app.directive("state", function() {
+    return {
+        restrict: "E",
+        controller: function() {
+            this.makeLaw = function(law) {
+                console.log("Law: " + law)
+            }
+        },
+        link: function (scope, element, attrs, countryCtrl) {
+
+        }
+    }
 });
+
+app.directive("city", function() {
+    return {
+        restrict: "E",
+        require: ["^country", "^state"],
+        link: function(scope, element, attrs, ctrls) {
+            ctrls[0].makeAnnouncement("This city rocks.");
+            ctrls[1].makeLaw("Jumper higher");
+        }
+    }
+});
+
